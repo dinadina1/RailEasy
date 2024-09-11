e2df4967f8fe0df5ee89d83564d88b3db5d6e661
dinadina1 <dinad9355@gmail.com>
2024-09-11 09:32:29 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://3.84.31.96:8000", "http://localhost:8000", "http://localhost:5173"],
+    origin: ["http://3.84.31.96:8000", "http://localhost:8000", "http://localhost:5173", "http://3.84.31.96/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
